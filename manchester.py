9b877d8bc3256561144713f29b602f6e1a60a7bb
Conke94 <gabrielmoroconke@hotmail.com>
2024-06-25 15:58:32 -0300

    fix: strings with accentuation

--- manchester.py
+++ manchester.py
@@ -1,11 +1,45 @@
+# -*- coding: utf-8 -*-
 import plotly.graph_objects as go
 from tkinter import ttk
 import tkinter as tk
 import socket
 
-def generateGraph(bits):
-    encoded_signal = differential_manchester_encoding(bits)
+def controller(event):
+    str_inserted = entry.get()
+    encrypted_value = encrypt(str_inserted)
+    binary_value = stringToBits(encrypted_value)
+    result_label.config(text=f"Mensagem escrita: {str_inserted}\nMensagem encriptada: {encrypted_value}\nMensagem em binário: {binary_value}")
+    encoded_signal = differentialManchesterEncoding(binary_value)
+    generateGraph(encoded_signal)
+    sendMessage(encoded_signal, 'localhost')
 
+def encrypt(str_inserted):
+    message = str_inserted
+    key = 1
+    characters = 'AaáàãâBbCcçDdEeéèêFfGgHhIiíìîJjKkLlMmNnOoóòõôPpQqRrSsTtUuúùûVvWwXxYyZz'
+    encrypted_value = ''
+    for character in message:
+        if character in characters:
+            num = characters.find(character)
+            num = num + key
+            if num >= len(characters):
+                num = num - len(characters)
+            encrypted_value = encrypted_value + characters[num]
+        else:
+            encrypted_value = encrypted_value + character
+    return encrypted_value
+
+def stringToBits(str):
+    string_in_bits = []
+    for letter in str:
+        ascii_value = ord(letter)
+        binary_value = bin(ascii_value)[2:]
+        binary_value.zfill(8)
+        for bit in binary_value:
+            string_in_bits.append(bit)
+    return string_in_bits
+
+def generateGraph(encoded_signal):
     x = list(range(len(encoded_signal)))
     fig = go.Figure()
 
@@ -30,7 +64,7 @@ def generateGraph(bits):
 
     fig.show()
 
-def differential_manchester_encoding(bits):
+def differentialManchesterEncoding(bits):
     encoded_signal = []
     current_level = '1'
 
@@ -56,44 +90,6 @@ def differential_manchester_encoding(bits):
     
     return encoded_signal
 
-def string_to_bits(str):
-    string_in_bits = []
-    for letter in str:
-        ascii_value = ord(letter)
-        binary_value = bin(ascii_value)[2:]
-        binary_value.zfill(8)
-        for bit in binary_value:
-            string_in_bits.append(bit)
-    return string_in_bits
-
-#Caeser Cipher
-def encrypt(str_inserted):
-    message = str_inserted
-    key = 1
-    characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    encrypted_value = ''
-    message = message.upper()
-    for character in message:
-        if character in characters:
-            num = characters.find(character)
-            num = num + key
-            if num >= len(characters):
-                num = num - len(characters)
-            elif num < 0:
-                num = num + len(characters)
-            encrypted_value = encrypted_value + characters[num]
-        else:
-            encrypted_value = encrypted_value + character
-    return encrypted_value
-
-def enterPressed(event):
-    str_inserted = entry.get()
-    encrypted_value = encrypt(str_inserted)
-    binary_value = string_to_bits(encrypted_value)
-    result_label.config(text=f"Mensagem escrita: {str_inserted}\nMensagem encriptada: {encrypted_value}\nMensagem em binário: {binary_value}")
-    generateGraph(binary_value)
-    sendMessage(differential_manchester_encoding(binary_value), 'localhost')
-
 def sendMessage(message, host, port=1234):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((host, port))
@@ -106,12 +102,13 @@ def sendMessage(message, host, port=1234):
     finally:
         client_socket.close()
 
-window = tk.Tk()
-entry_label = ttk.Label(window, text = "Digite a mensagem: ")
-entry_label.pack()
-entry = ttk.Entry(window, width=40)
-entry.pack()
-entry.bind('<Return>', enterPressed)
-result_label = ttk.Label(window, text="")
-result_label.pack(pady=10)
-window.mainloop()
+if __name__ == "__main__":
+    window = tk.Tk()
+    entry_label = ttk.Label(window, text = "Digite a mensagem: ")
+    entry_label.pack()
+    entry = ttk.Entry(window, width=40)
+    entry.pack()
+    entry.bind('<Return>', controller)
+    result_label = ttk.Label(window, text="")
+    result_label.pack(pady=10)
+    window.mainloop()
